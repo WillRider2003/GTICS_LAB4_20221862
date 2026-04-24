72a001771c1cc44079fc545f65ab079d7159d0b9
SergioAnderson123 <a20223291@pucp.edu.pe>
2026-04-23 19:14:04 -0500

    commit inicial

---
+++ src/main/java/org/example/apisgtics/ApisgticsApplication.java
@@ -0,0 +1,13 @@
+package org.example.apisgtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisgticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApisgticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apisgtics/ApisgticsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apisgtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisgticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
